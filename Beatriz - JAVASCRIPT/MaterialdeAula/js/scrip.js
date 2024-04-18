ce8c38ed84b8df0dbf3bee8cf23499caaeea3d54
Iam-BeatrizM <157439476+Iam-BeatrizM@users.noreply.github.com>
2024-04-18 10:03:09 -0300

    Material_de_Aula
    JavaScript

---
+++ Beatriz - JAVASCRIPT/MaterialdeAula/js/scrip.js
@@ -0,0 +1,18 @@
+console.log("Olá mundo")
+
+// //caixas de texto (input)
+// //prompt = receber um texto 
+// window.prompt("Qual é o seu nome?");
+
+// //alert = manda uma mensagem 
+// window.alert("FRONT É TOP!");
+
+// //confirm = pergunta podendo sair verdadeiro o falso 
+// window.confirm("Deseja baixar arquivo novamente?");
+
+var name = "Uma variavel teste!";
+var ligado = true; 
+var numero = 91; 
+
+// TYPEOF (no Inspecionar) = verifica o tipo da variavel        
+
